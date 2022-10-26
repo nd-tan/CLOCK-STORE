ad062429b4e9f48df3fa05634751d87fd48ebe75
demaitinh1993 <tntyesterday031093@gmail.com>
2022-10-26 08:58:10 +0700

    category-controller-service-repositories

--- BackEnd/app/Http/Controllers/Admin/CategoryController.php
+++ BackEnd/app/Http/Controllers/Admin/CategoryController.php
@@ -3,9 +3,92 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Services\Category\CategoryServiceInterface;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CategoryController extends Controller
 {
-    //
+    private $categoryService;
+    public function __construct(CategoryServiceInterface $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
+    public function index(Request $request)
+    {
+        $categories = $this->categoryService->all($request);
+        return view('#',compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('#');
+    }
+    
+    public function store(StoreCategoryRequest $request)
+    {
+        $data = $request->all();
+        $this->categoryService->create($data);
+        $notification = array(
+            'message' => 'Thêm danh mục thành công!',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('#')->with($notification);
+    }
+
+    public function edit($id)
+    {
+        $category = $this->categoryService->find($id);
+        return view('#',compact('category'));
+    }
+
+    public function update(UpdateCategoryRequest $request,$id)
+    {
+        $data = $request->all();
+        $this->categoryService->update( $id, $data);
+        $notification = array(
+            'message' => 'Sửa danh mục thành công!',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('#')->with($notification);
+
+    }
+
+    public function destroy($id)
+    {
+        $category = $this->categoryService->delete( $id);
+        return response()->json($category);
+    }
+
+    public function getTrashed(){
+        $categories = $this->categoryService->getTrashed();
+        return view('#',compact('categories'));
+    }
+
+    public function restore($id){
+        $this->categoryService->restore($id);
+        $notification = array(
+            'message' => 'Khôi phục danh mục thành công',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('')->with($notification);
+    }
+
+    public function force_destroy($id){
+        try {
+            $category = $this->categoryService->force_destroy( $id);
+            return response()->json($category);
+        } catch (Exception $e) {
+            Log::error('errors' . $e->getMessage() . ' getLine' . $e->getLine());
+            $notification = array(
+                'message' => 'Không thể xóa vì có sản phẩm thuộc danh mục này!',
+                'alert-type' => 'error'
+            );
+            return redirect()->route('')->with($notification);
+        }
+    }
 }
--- BackEnd/routes/web.php
+++ BackEnd/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +19,19 @@ Route::get('/', function () {
 });
 Route::get('/cate', function () {
     return view('admin.categories.index');
+});
+Route::prefix('categories')->group(function(){
+    Route::get('/',[CategoryController::class,'index'])->name('category.index');
+    ////add
+    Route::get('/add',[CategoryController::class,'create'])->name('category.add');
+    Route::post('/store',[CategoryController::class,'store'])->name('category.store');
+    ///edit
+    Route::get('/edit/{id}',[CategoryController::class,'edit'])->name('category.edit');
+    Route::put('/update/{id}',[CategoryController::class,'update'])->name('category.update');
+    ////xóa
+    Route::delete('/delete/{id}',[CategoryController::class,'force_destroy'])->name('category.delete');
+    /////xóa mềm
+    Route::delete('/destroy/{id}',[CategoryController::class,'destroy'])->name('category.destroy');
+    Route::get('/getTrashed',[CategoryController::class,'getTrashed'])->name('category.getTrashed');
+    Route::get('/restore/{id}',[CategoryController::class,'restore'])->name('category.restore');
 });
