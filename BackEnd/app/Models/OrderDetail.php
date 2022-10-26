c96e7e1d9522054f0c72573049c14dc53c11f17b
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-10-26 08:59:06 +0700

    cus-order-orderdetail

--- BackEnd/app/Models/OrderDetail.php
+++ BackEnd/app/Models/OrderDetail.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Customer extends Model
+class OrderDetail extends Model
 {
     use HasFactory;
-    public function orders()
-    {
-        return $this->hasMany(Order::class, 'customer_id', 'id');
-    }
 }
