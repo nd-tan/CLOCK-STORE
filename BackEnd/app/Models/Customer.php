64ac1c65f39128a27b6eb6da83a5edd2e38dfd79
MaiXuanCuong <maixuancuong2906@gmail.com>
2022-10-31 14:12:07 +0700

    customer-customerapi-upadate

--- BackEnd/app/Models/Customer.php
+++ BackEnd/app/Models/Customer.php
@@ -4,14 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Customer extends Model
+class Customer extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    use HasFactory,HasApiTokens;
     use Notifiable;
     use SoftDeletes;
+    protected $fillable = [
+        'id','name','email','phone','password','created_at'
+    ];
     public function orders()
     {
         return $this->hasMany(Order::class, 'customer_id', 'id');
@@ -47,5 +53,17 @@ class Customer extends Model
             $query->where('customers.email','like', '%' . $request->email . '%' );
         };
         return $query;
+    }
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
     }
 }
